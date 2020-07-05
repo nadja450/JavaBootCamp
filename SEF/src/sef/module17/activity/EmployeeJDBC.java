87f417d27e32e7d6cfc5dcc8f38461d155408513
Nadezda Dubina <nadja_year2003@yahoo.com>
2020-07-05 15:23:26 +0300

    Activity_Day5

--- SEF/src/sef/module17/activity/EmployeeJDBC.java
+++ SEF/src/sef/module17/activity/EmployeeJDBC.java
@@ -8,13 +8,15 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class EmployeeJDBC {
-
+	
+	
+	
 	public Connection createConnection()
 	{
 		Connection con=null;
 		String url = "jdbc:mysql://localhost/activity";
 		String user = "root";
-		String pass = "adbd1234";
+		String pass = "Nadja123";
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -31,103 +33,158 @@ public class EmployeeJDBC {
 		return con;
 	}
 	
-	public Employee findEmployeeById(String id)
+	
+	
+        public Employee findEmployeeById(String id)
 	{
-		Connection con = createConnection();
-		Employee emp=null;
-		try {
+        	Connection con = createConnection();
+        	Employee emp=null;
+			try {
+				// 1 - Create a PreparedStatement with a query
+			PreparedStatement pStnt=con.prepareStatement ("select * from employee where id=? ");
+//
+//		// 2 - Search for the given id
+		    pStnt.setString (1, id);
+//		// 3 - Execute this query
+//		
+             ResultSet rs=pStnt.executeQuery();
+//		// 4 - If resultset is not null, then initialize emp object with data 
+             if (rs.next())
+             {
+            emp=new Employee();
+            emp.setId (rs.getString((1)));
+            emp.setFirstName(rs.getString(2));
+            emp.setLastName(rs.getString(3));
+			emp.setSalary (Integer.parseInt (rs.getString((4))));
+				
+             }
+             con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+			return emp;
+		}
+	      
+        public ArrayList<Employee> findEmployeesByName(String name)
+        {
+        Connection con = createConnection();
+        ArrayList<Employee> list = new ArrayList<Employee>();
+//		
+         try {
+
+        	 PreparedStatement pStnt=con.prepareStatement ("select * from employee where firstName like ? or lastName like ?");
+//		// 1 - Create a PreparedStatement with a query
+            pStnt.setString (1,"%" + name + "%");
+            pStnt.setString (2,"%" + name + "%");
+//
+//		// 2 - Search for the given id
+//		
+//		// 3 - Execute this query
+           ResultSet rs=pStnt.executeQuery();
+//		
+//		// 4 - While there are some records, continue 
+            while (rs.next())
+           {			
+            	Employee emp=new Employee();
+//			
+            	emp.setId (rs.getString((1)));
+            	emp.setFirstName(rs.getString(2));
+            	emp.setLastName(rs.getString(3));
+            	emp.setSalary (Integer.parseInt (rs.getString((4))));
+//			
+            	list.add(emp);
+           }
+//		
+//			
+            con.close();
+         } 		catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		}
+		
+         return list;
+        }
+
+        public ArrayList<Employee> findEmployeesBySalary(int salary)
+      {
+        	Connection con = createConnection();
+        	ArrayList<Employee> list = new ArrayList<Employee>();
+		
+        	try {
 		// 1 - Create a PreparedStatement with a query
-		
-
-		// 2 - Search for the given id
-		
-
-		// 3 - Execute this query
-		
-		
-		// 4 - If resultset is not null, then initialize emp object with data 
-		
-		con.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return emp;
-	}
-
-	public ArrayList<Employee> findEmployeesByName(String name)
-	{
-		Connection con = createConnection();
-		ArrayList<Employee> list = new ArrayList<Employee>();
-		
-		try {
-		// 1 - Create a PreparedStatement with a query
-		
-
-		// 2 - Search for the given id
-		
-		// 3 - Execute this query
-		
-		
-		// 4 - While there are some records, continue 
-		
-			con.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return list;
-	}
-
-	public ArrayList<Employee> findEmployeesBySalary(int salary)
-	{
-		Connection con = createConnection();
-		ArrayList<Employee> list = new ArrayList<Employee>();
-		
-		try {
-		// 1 - Create a PreparedStatement with a query
-		
-
-		// 2 - Search for the given salary
-		
-
-		// 3 - Execute this query
-
-		
-		// 4 - While there are records, continue 
-
-		con.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return list;
-	}
-
-	public void insertEmployee(Employee emp)
-	{
-		Connection con = createConnection();
-		
-		//1 - Create a PreparedStatement with a query "insert into employee values(?,?,?,?)" 
-		
-		con.setAutoCommit(false);
-
-		//	Substitute the ? now.
-		
-		//2 - Execute this query using executeUpdate()
-			
-		System.out.println(rows + " row(s) added!");
-		con.commit();
-		con.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-
-	}
-
+        		PreparedStatement pStnt=con.prepareStatement ("select * from employee where salary = ?");
+//
+//		// 2 - Search for the given salary
+//		
+        		pStnt.setInt (1, salary);
+//			
+//		// 3 - Execute this query
+//
+        		ResultSet rs=pStnt.executeQuery();
+//		// 4 - While there are records, continue 
+        		while (rs.next())
+        		{			
+        			Employee emp=new Employee();
+//				
+        			emp.setId (rs.getString((1)));
+        			emp.setFirstName(rs.getString(2));
+        			emp.setLastName(rs.getString(3));
+        			emp.setSalary (Integer.parseInt (rs.getString((4))));
+//			
+        			list.add(emp);
+        			}
+//
+        			con.close();
+        	} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+        		e.printStackTrace();
+        	}
+//		
+        	return list;
+      		}
+//
+        public void insertEmployee(Employee emp)
+        	{
+            Connection con = createConnection();
+//		
+//		//1 - Create a PreparedStatement with a query "insert into employee values(?,?,?,?)" 
+                        
+            try {
+//
+            PreparedStatement pStnt=con.prepareStatement ("insert into employee values (?,?,?,?)");
+			          
+            																	
+	        con.setAutoCommit(false);
+	        pStnt.setString (1,emp.getId());
+            pStnt.setString (2,emp.getFirstName());
+            pStnt.setString (3,emp.getLastName());
+            pStnt.setInt (4,emp.getSalary());
+            
+            
+            int rows = pStnt.executeUpdate();
+	        
+	       	//	Substitute the ? now.
+//		
+//		//2 - Execute this query using executeUpdate()
+//			
+            System.out.println(rows + " row(s) added!");
+            con.commit();
+            con.close();
+        	} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+        		e.printStackTrace();
+        	}
+//		
 }
+
+
+	
+}
+
+
+
+
+//
+//
--- SEF/src/sef/module17/activity/EmployeeJDBCTest.java
+++ SEF/src/sef/module17/activity/EmployeeJDBCTest.java
@@ -7,6 +7,7 @@ import junit.framework.TestCase;
 
 public class EmployeeJDBCTest extends TestCase {
 
+
 	EmployeeJDBC emp;
 	
 	protected void setUp() throws Exception {
@@ -17,36 +18,86 @@ public class EmployeeJDBCTest extends TestCase {
 	public void testCreateConnection() {
 		Connection con = emp.createConnection();
 		assertNotNull(con);
-		
+	
 	}
 
 	public void testFindEmployeeById_MatchFound() {
 		//complete the code here
-	}
-
+		Employee e1=new Employee();
+		e1=emp.findEmployeeById("3");
+		
+		assertNotNull (e1);
+				 	
+    }
+         	
+	   
 	public void testFindEmployeeById_NoMatchFound() {
 		//complete the code here
+		Employee ee = emp.findEmployeeById("99");
+		assertNull (ee);
+	
 	}
 	
 	
 	public void testFindEmployeeByName_MatchFound() {
-		//complete the code here
+		
+		ArrayList<Employee> e1= emp.findEmployeesByName("Vera");
+		
+		
+		assertNotNull (e1);
+		
+		
+				
+			//complete the code here
 	}
 	
 	public void testFindEmployeeByName_NoMatchFound() {
 		//complete the code here
+		ArrayList<Employee> e1=emp.findEmployeesByName("Vasiljeva");
+		//Employee ee = emp.findEmployeesByName("Sidorov");
+		assertNull(e1);
+		
 	}
 
 	public void testFindEmployeeBySalary_MatchFound() {
 		//complete the code here
+		ArrayList<Employee> e1= emp.findEmployeesBySalary(3000);
+		assertNotNull (e1);
+				
 	}
 
 	public void testFindEmployeeBySalary_NoMatchFound() {
 		//complete the code here
+		ArrayList<Employee> e1= emp.findEmployeesBySalary(1000);
+		assertNull (e1);
+		
+
 	}
 	
-	public void testInsertEmployee(){
-		//complete the code here
+    public void testInsertEmployee(){
+       Employee e1=new Employee();
+       
+       e1.setId("11");
+       e1.setFirstName("Oleg");
+       e1.setLastName("Ivanov");
+       e1.setSalary(5500);
+       
+       emp.insertEmployee(e1);	
+       
+        
+	   Employee e2=new Employee();
+	   e2=emp.findEmployeeById("11");
+	   		//Employee ee = emp.findEmployeeById("3");
+		assertNotNull (e2);	
+					 	
+     }	   
+	   
 		
-	}
-}
+			       
+}	
+		
+
+		//complete the code here
+    		 
+	
+
